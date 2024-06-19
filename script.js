cf98e153fd7d680a9463b8e206cd1576600144a9
Aravinth <153979321+aravinthnataraj@users.noreply.github.com>
2024-06-19 15:49:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+function iconplus() {
+  const showplusEl = document.querySelector("#icon-plus");
+  showplusEl.classList.add("plus-hide");
+  const showparaEl = document.querySelector("#big-para");
+  showparaEl.classList.remove("big-para");
+  const showminuesEl = document.querySelector("#icon-minus");
+  showminuesEl.classList.remove("minus-hide");
+  showminuesEl.classList.add("minus-show");
+}
+
+function iconminus() {
+  const showminuesEl = document.querySelector("#icon-minus");
+  showminuesEl.classList.add("minus-hide");
+  const showplusEl = document.querySelector("#icon-plus");
+  showplusEl.classList.remove("plus-hide");
+  showplusEl.classList.add("plus-show");
+  const showparaEl = document.querySelector("#big-para");
+  showparaEl.classList.add("big-para");
+}
+
+function iconplus1() {
+  const showplusEl = document.querySelector("#icon-plus1");
+  showplusEl.classList.add("plus-show");
+  const showminuesEl = document.querySelector("#icon-minus1");
+  showminuesEl.classList.remove("minus-hide");
+  showminuesEl.classList.add("minus-show");
+  const showparaEl = document.querySelector("#big-para1");
+  showparaEl.classList.remove("big-para1");
+  showplusEl.classList.add("plus-hide");
+}
+
+function iconminus1() {
+  const showminuesEl = document.querySelector("#icon-minus1");
+  showminuesEl.classList.add("minus-hide");
+  const showplusEl = document.querySelector("#icon-plus1");
+  showplusEl.classList.add("plus-show");
+  showplusEl.classList.remove("plus-hide");
+  const showparaEl = document.querySelector("#big-para1");
+  showparaEl.classList.add("big-para1");
+}
+
+function iconplus2() {
+  const showplusEl = document.querySelector("#icon-plus2");
+  showplusEl.classList.add("plus-show");
+  const showminuesEl = document.querySelector("#icon-minus2");
+  showminuesEl.classList.remove("minus-hide");
+  showminuesEl.classList.add("minus-show");
+  const showparaEl = document.querySelector("#big-para2");
+  showparaEl.classList.remove("big-para2");
+  showplusEl.classList.add("plus-hide");
+}
+
+function iconminus2() {
+  const showminuesEl = document.querySelector("#icon-minus2");
+  showminuesEl.classList.add("minus-hide");
+  const showplusEl = document.querySelector("#icon-plus2");
+  showplusEl.classList.add("plus-show");
+  showplusEl.classList.remove("plus-hide");
+  const showparaEl = document.querySelector("#big-para2");
+  showparaEl.classList.add("big-para2");
+}
+
+function iconplus3() {
+  const showplusEl = document.querySelector("#icon-plus3");
+  showplusEl.classList.add("plus-show");
+  const showminuesEl = document.querySelector("#icon-minus3");
+  showminuesEl.classList.remove("minus-hide");
+  showminuesEl.classList.add("minus-show");
+  const showparaEl = document.querySelector("#big-para3");
+  showparaEl.classList.remove("big-para3");
+  showplusEl.classList.add("plus-hide");
+}
+
+function iconminus3() {
+  const showminuesEl = document.querySelector("#icon-minus3");
+  showminuesEl.classList.add("minus-hide");
+  const showplusEl = document.querySelector("#icon-plus3");
+  showplusEl.classList.add("plus-show");
+  showplusEl.classList.remove("plus-hide");
+  const showparaEl = document.querySelector("#big-para3");
+  showparaEl.classList.add("big-para3");
+}
